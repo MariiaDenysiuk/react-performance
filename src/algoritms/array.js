213e9171f2dc74925e18d89ba013d78d5b08f823
Mariia <marydenysiuk@gmail.com>
2019-12-24 17:23:42 +0200

    algoritms

--- src/algoritms/array.js
+++ src/algoritms/array.js
@@ -8,19 +8,39 @@ const sorted = ar.sort();
 console.log(sorted[1]);
 
 //Find first not repeated numbers
-
-//effective
 function firstNonRepeating(arr, n) {
     let myMap = new Map();
     for(let i = 0; i < n; i++) {
-        if(myMap.get(arr[i])) {
-           myMap.set(arr[i], )
+        let getData =  myMap.get(arr[i]);
+        if(getData) {
+           myMap.set(arr[i], getData + 1);
         } else {
-            myMap.set(arr[i], 1);
+           myMap.set(arr[i], 1);
         }
     }
+
+    for(let i = 0; i < n; i++) {
+        if(myMap.get(arr[i]) === 1) {
+            return arr[i];
+        }
+    }
+
+    return -1;
 }
 
-function main() {
-
+function main(data) {
+    let ar = data;
+    let len = ar.length;
+    let find = firstNonRepeating(ar, len);
+    console.log(find);
 }
+
+main([9, 4, 9, 6, 7, 4, 6]);
+
+// concat array
+const arr1 = [4, 8, 12, 14, 23, 85];
+const arr2 = [2, 4, 8, 9, 12, 16];
+const sortedArr = arr1.concat(arr2).sort(function (a, b) {
+    return a - b;
+});
+console.log(sortedArr);
